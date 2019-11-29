b9e8d42f2526ed4a2329bbe9baed45992cc27e13
Boris Đemrovski <djboris88@gmail.com>
2019-11-29 22:30:11 +0100

    Setup completed

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -f "~/ubuntu-setup.log" ]]
+	then
+		echo "Setup already run. Please check the log at '~/ubuntu-setup.log'."
+		exit 0
+fi
 
 # ########
 # Get user inputs
@@ -11,37 +16,48 @@ read -p 'Your Email address: ' emailvar
 # ########
 # Add repositories
 # ########
-add-apt-repository -y ppa:ondrej/php
-add-apt-repository -y ppa:phpmyadmin/ppa
-apt update
-apt upgrade -y
+echo "Adding apt repositories and installing general requirements..."
+add-apt-repository -y ppa:ondrej/php &> ~/ubuntu-setup.log
+add-apt-repository -y ppa:phpmyadmin/ppa &>> ~/ubuntu-setup.log
+apt update &>> ~/ubuntu-setup.log
+apt upgrade -y &>> ~/ubuntu-setup.log
 
 # install general requirements
-apt install -y software-properties-common gdebi curl git libfreetype6-dev libjpeg-dev libmagickwand-dev libpng-dev libzip-dev
+apt install -y software-properties-common gdebi curl git libfreetype6-dev libjpeg-dev libmagickwand-dev libpng-dev libzip-dev &>> ~/ubuntu-setup.log
 
 
 # ########
 # Setup Apache, PHP, MySQL
 # ########
 
-# install apache2
-apt install -y apache2
+echo "Installing Apache, PHP and MySQL..."
 
-systemctl start apache2
-systemctl enable apache2
+# install apache2
+apt install -y apache2 &>> ~/ubuntu-setup.log
+
+systemctl start apache2 &>> ~/ubuntu-setup.log
+systemctl enable apache2 &>> ~/ubuntu-setup.log
 
 # setup permissions for /var/www
-gpasswd -a "$(users)" www-data
-chown -R www-data:www-data /var/www
-setfacl -Rdm g:www-data:rwx /var/www
-setfacl -Rdm u:www-data:rwx /var/www
-setfacl -Rdm "u:$(users):rwx" /var/www
+gpasswd -a "$(users)" www-data &>> ~/ubuntu-setup.log
+chown -R www-data:www-data /var/www &>> ~/ubuntu-setup.log
+setfacl -Rdm g:www-data:rwx /var/www &>> ~/ubuntu-setup.log
+setfacl -Rdm u:www-data:rwx /var/www &>> ~/ubuntu-setup.log
+setfacl -Rdm "u:$(users):rwx" /var/www &>> ~/ubuntu-setup.log
 
 # set root for 'localhost' to /var/www
-sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www/g" /etc/apache2/sites-available/000-default.conf
+sed -i "s/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www/g" /etc/apache2/sites-available/000-default.conf &>> ~/ubuntu-setup.log
+
+# setup vhost script
+echo "Setting up a script for automatic creation of virtual hosts..."
+echo "To use it, type 'vhost create' in your terminal and follow the instructions."
+echo "To remove existing virtual host, type 'vhost delete' and type in the vhost address."
+wget https://raw.githubusercontent.com/djboris88/ubuntu-setup/master/virtualhost.sh &>> ~/ubuntu-setup.log
+mv virtualhost.sh /usr/bin/vhost &>> ~/ubuntu-setup.log
+chmod +x /usr/bin/vhost &>> ~/ubuntu-setup.log
 
 # install php 7.3
-apt install -y php7.3-fpm php7.3-common php7.3-zip php7.3-curl php7.3-xml php7.3-xmlrpc php7.3-json php7.3-mysql php7.3-pdo php7.3-gd php7.3-imagick php7.3-ldap php7.3-imap php7.3-mbstring php7.3-intl php7.3-cli php7.3-recode php7.3-tidy php7.3-bcmath php7.3-opcache php7.3-xdebug
+apt install -y php7.3-fpm php7.3-common php7.3-zip php7.3-curl php7.3-xml php7.3-xmlrpc php7.3-json php7.3-mysql php7.3-pdo php7.3-gd php7.3-imagick php7.3-ldap php7.3-imap php7.3-mbstring php7.3-intl php7.3-cli php7.3-recode php7.3-tidy php7.3-bcmath php7.3-opcache php7.3-xdebug &>> ~/ubuntu-setup.log
 
 # configure php
 declare -A confs=(
@@ -61,7 +77,7 @@ do
 	search=$i
 	replace=${confs[$i]}
 
-	sed -i -E "s/.?(${search}).+/\1 = ${replace}/g" /etc/php/7.3/fpm/php.ini
+	sed -i -E "s/.?(${search}).+/\1 = ${replace}/g" /etc/php/7.3/fpm/php.ini  &>> ~/ubuntu-setup.log
 done
 
 # configure opcache
@@ -81,18 +97,18 @@ xdebug.remote_mode = req
 xdebug.remote_port = 9000" >> /etc/php/7.3/mods-available/xdebug.ini
 
 # setup mysql and phpmyadmin
-apt install -y mysql-server
-apt install -y phpmyadmin
+apt install -y mysql-server &>> ~/ubuntu-setup.log
+apt install -y phpmyadmin &>> ~/ubuntu-setup.log
 
 # enable no password for phpmyadmin
-sed -i "s/\/\/ \$cfg\[\x27Servers\x27\]\[\$i\]\[\x27AllowNoPassword\x27\] = TRUE;/\$cfg\[\x27Servers\x27\]\[\$i\]\[\x27AllowNoPassword\x27\] = TRUE;/g" /etc/phpmyadmin/config.inc.php
+sed -i "s/\/\/ \$cfg\[\x27Servers\x27\]\[\$i\]\[\x27AllowNoPassword\x27\] = TRUE;/\$cfg\[\x27Servers\x27\]\[\$i\]\[\x27AllowNoPassword\x27\] = TRUE;/g" /etc/phpmyadmin/config.inc.php &>> ~/ubuntu-setup.log
 
 # enable everything and restart apache
-a2enmod proxy_fcgi setenvif
-a2enconf php7.3-fpm
+a2enmod proxy_fcgi setenvif &>> ~/ubuntu-setup.log
+a2enconf php7.3-fpm &>> ~/ubuntu-setup.log
 
-systemctl restart apache2
-systemctl restart php7.3-fpm
+systemctl restart apache2 &>> ~/ubuntu-setup.log
+systemctl restart php7.3-fpm &>> ~/ubuntu-setup.log
 
 
 # ########
@@ -100,23 +116,25 @@ systemctl restart php7.3-fpm
 # ########
 
 # install Composer
-curl -sS https://getcomposer.org/installer |php
-mv composer.phar /usr/local/bin/composer
+echo "Installing Composer..."
+curl -sS https://getcomposer.org/installer | php &>> ~/ubuntu-setup.log
+mv composer.phar /usr/local/bin/composer &>> ~/ubuntu-setup.log
 
 # install NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash
+echo "Installing NVM, Node.js, npm and gulp..."
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash &>> ~/ubuntu-setup.log
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # setup Node.js and global npm modules
-nvm install 10.15.3
-npm i -g gulp-cli yarn
+nvm install 10.15.3 &>> ~/ubuntu-setup.log
+npm i -g gulp-cli yarn &>> ~/ubuntu-setup.log
 
 # setup GIT
-sudo -u $(users) git config --global user.name "${namevar}"
-sudo -u $(users) git config --global user.email "${emailvar}"
+sudo -u $(users) git config --global user.name "${namevar}" &>> ~/ubuntu-setup.log
+sudo -u $(users) git config --global user.email "${emailvar}" &>> ~/ubuntu-setup.log
 
 
 # ########
@@ -124,33 +142,73 @@ sudo -u $(users) git config --global user.email "${emailvar}"
 # ########
 
 # code editors/IDEs
-snap install sublime-text --classic
-snap install code --classic
-snap install phpstorm --classic
+echo "Installing Sublime Text, VSCode and PHPStorm..."
+snap install sublime-text --classic &>> ~/ubuntu-setup.log
+snap install code --classic &>> ~/ubuntu-setup.log
+snap install phpstorm --classic &>> ~/ubuntu-setup.log
 
 # Skype
-snap install skype --classic
+echo "Installing Skype..."
+snap install skype --classic &>> ~/ubuntu-setup.log
+
+# autostart
+echo "[Desktop Entry]
+Name=skype
+Icon=skype
+Exec=skype
+Terminal=false
+Type=Application
+X-GNOME-Autostart-enabled=true" > ~/.config/autostart/skype.desktop
 
 # Clipboard manager
-apt install -y copyq default-jre
+echo "Installing CopyQ, a nice clipboard manager..."
+echo "Shortcut for using it is 'Ctrl + \`'"
+apt install -y copyq default-jre xbindkeys &>> ~/ubuntu-setup.log
+
+# autostart
+echo "[Desktop Entry]
+Name=CopyQ
+Icon=copyq
+Exec=copyq
+Terminal=false
+Type=Application
+X-GNOME-Autostart-enabled=true" > ~/.config/autostart/copyq.desktop
+
+# add keybinding
+sudo -u $(users) echo '"copyq menu"
+Control+Mod2 + grave' >> ~/.xbindkeysrc
 
 # Screenshot tool
-apt install -y flameshot xbindkeys
-gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot ['']
+echo "Installing Flameshot, nice screenshot app..."
+apt install -y flameshot  &>> ~/ubuntu-setup.log
+
+# autostart
+echo "[Desktop Entry]
+Name=flameshot
+Icon=flameshot
+Exec=flameshot
+Terminal=false
+Type=Application
+X-GNOME-Autostart-enabled=true" > ~/.config/autostart/Flameshot.desktop
+
+# remove system keybinding and add custom
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot [''] &>> ~/ubuntu-setup.log
 sudo -u $(users) echo '"flameshot gui"
-Mod2 + Print' > ~/.xbindkeysrc
+Mod2 + Print' >> ~/.xbindkeysrc
 
 # Chrome
+echo "Installing Google Chrome..."
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >  /etc/apt/sources.list.d/google-chrome.list
-wget -qO - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-apt update
-apt install -y google-chrome-stable
+wget -qO - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - &>> ~/ubuntu-setup.log
+apt update &>> ~/ubuntu-setup.log
+apt install -y google-chrome-stable &>> ~/ubuntu-setup.log
 
 # git client GitEye
-wget -O giteye.zip https://www.collab.net/sites/default/files/downloads/GitEye-2.2.0-linux.x86_64.zip
-unzip -o giteye.zip -d /opt/giteye
-rm giteye.zip
-ln -sf /opt/giteye/GitEye /usr/bin/GitEye
+echo "Installing GitEye, git client with gui..."
+wget -O giteye.zip https://www.collab.net/sites/default/files/downloads/GitEye-2.2.0-linux.x86_64.zip &>> ~/ubuntu-setup.log
+unzip -o giteye.zip -d /opt/giteye &>> ~/ubuntu-setup.log
+rm -f giteye.zip &>> ~/ubuntu-setup.log
+ln -sf /opt/giteye/GitEye /usr/bin/GitEye &>> ~/ubuntu-setup.log
 echo "[Desktop Entry]
 Version=2.2.0
 Name=GitEye
@@ -164,4 +222,5 @@ Icon=/opt/giteye/icon.xpm
 Name[en_US]=GitEye" > /usr/share/applications/giteye.desktop
 
 # reboot
-reboot
+echo "Rebooting in 3..."
+sleep 3 && reboot
